350903bd7d25b819e3944899c3970c9883c0ec06
Stupishin Daniil <stupishin1597@yandex.ru>
2024-05-08 19:55:14 +0300

    version 1.0.4 -- fixing users

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,9 @@
 const sendAllUsers = (req, res) => {
   // Установим заголовок ответа в формате JSON
-  res.setHeader("Content-Type", "application/json");
-  // Отправим данные в виде JSON-объекта,
-  // которые подготовим в миддлваре findAllCategories
-  res.end(JSON.stringify(req.categoriesArray));
+res.setHeader('Content-Type', 'application/json');
+// Отправим данные в виде JSON-объекта, 
+// которые подготовим в миддлваре findAllUsers
+res.end(JSON.stringify(req.usersArray));
 };
 
 // Экспортируем контроллер
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,12 @@
-// Создаём роут для запросов категорий
-const usersRouter = require("express").Router();
+// Создаём роут для запросов пользователей 
+const usersRouter = require('express').Router();
 
 // Импортируем вспомогательные функции
-const findAllUsers = require("../middlewares/users");
-const sendAllUsers = require("../controllers/users");
+const findAllUsers = require('../middlewares/users');
+const sendAllUsers = require('../controllers/users');
 
 // Обрабатываем GET-запрос с роутом '/users'
-usersRouter.get("/users", findAllUsers, sendAllUsers);
+usersRouter.get('/users', findAllUsers, sendAllUsers);
 
 // Экспортируем роут для использования в приложении — app.js
 module.exports = usersRouter;
