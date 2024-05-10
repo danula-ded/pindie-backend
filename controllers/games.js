5ac83bcafa571fdddec1019c8b2799ff5fd0d191
Stupishin Daniil <stupishin1597@yandex.ru>
2024-05-11 00:06:58 +0300

    deleted all

--- controllers/games.js
+++
@@ -1,9 +0,0 @@
-// Файл controllers/games.js
-
-const sendAllGames = (req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  // Вернём найденные игры в формате JSON
-  res.end(JSON.stringify(req.gamesArray));
-};
-
-module.exports = sendAllGames;
