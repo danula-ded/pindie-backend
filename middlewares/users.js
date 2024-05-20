46130706bb6278dd5f4c726d90fa467c9aaddbc1
Stupishin Daniil <stupishin1597@yandex.ru>
2024-05-20 22:29:01 +0300

    version 1.2.1 -- seved

--- middlewares/users.js
+++ middlewares/users.js
@@ -2,6 +2,7 @@
 
 // Импортируем модель
 const users = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 const findAllUsers = async (req, res, next) => {
   // По GET-запросу на эндпоинт /users найдём все документы пользователей
@@ -94,6 +95,22 @@ const checkEmptyNameAndEmail = async (req, res, next) => {
     next();
   }
 };
+
+// функция хэширования пароля
+const hashPassword = async (req, res, next) => {
+  try {
+    // Создаём случайную строку длиной в десять символов
+    const salt = await bcrypt.genSalt(10);
+    // Хешируем пароль
+    const hash = await bcrypt.hash(req.body.password, salt);
+    // Полученный в запросе пароль подменяем на хеш
+    req.body.password = hash;
+    next();
+  } catch (error) {
+    res.status(400).send({ message: "Ошибка хеширования пароля" });
+  }
+};
+
 // Экспортируем функцию поиска всех пользователей
 module.exports = {
   findAllUsers,
