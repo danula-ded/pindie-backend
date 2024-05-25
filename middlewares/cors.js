6dc0fa40ccecddaab18ff6c81f9d67db1a9f4fc1
Stupishin Daniil <stupishin1597@yandex.ru>
2024-05-25 14:26:54 +0300

    version 1.2.8 -- fixing cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,20 +1,22 @@
 const allowedCors = [
   "https://practicum.yandex.ru",
   "https://students-projects.ru",
-  "localhost:3000",
-  "localhost:3001",
+  "http://localhost:3000",
+  "http://localhost:3001",
 ];
 
 function cors(req, res, next) {
   const { origin } = req.headers;
 
-  res.header("Access-Control-Allow-Origin", origin);
-
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
   res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
   res.header(
     "Access-Control-Allow-Headers",
     "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
   );
+
   next();
 }
 
