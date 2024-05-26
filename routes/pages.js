b26d548fe471854109ea001de460978eb36cbe2b
Stupishin Daniil <stupishin1597@yandex.ru>
2024-05-26 10:03:18 +0300

    version 2.0.6 -- fixing

--- routes/pages.js
+++ routes/pages.js
@@ -1,8 +1,8 @@
 const pagesRouter = require("express").Router();
-const { sendDashboard } = require("../controllers/auth.js");
+const { sendIndex, sendDashboard } = require("../controllers/auth.js");
 const { checkAuth, checkCookiesJWT } = require("../middlewares/auth.js");
 
-pagesRouter.get("/", checkCookiesJWT, checkAuth, sendDashboard);
+pagesRouter.get("/", sendIndex);
 pagesRouter.get("/admin/**", checkCookiesJWT, checkAuth, sendDashboard);
 
 module.exports = pagesRouter;
