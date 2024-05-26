11f0ba05f05aa38a5e2d0ffc88e4e55527663012
Stupishin Daniil <stupishin1597@yandex.ru>
2024-05-26 09:54:27 +0300

    version 2.0.5 -- redirection admin

--- routes/pages.js
+++ routes/pages.js
@@ -2,6 +2,7 @@ const pagesRouter = require("express").Router();
 const { sendDashboard } = require("../controllers/auth.js");
 const { checkAuth, checkCookiesJWT } = require("../middlewares/auth.js");
 
+pagesRouter.get("/", checkCookiesJWT, checkAuth, sendDashboard);
 pagesRouter.get("/admin/**", checkCookiesJWT, checkAuth, sendDashboard);
 
 module.exports = pagesRouter;
